431500eda705c1c9e0234b192cbf88790fc36026
abha <abha@example.com>
2019-06-13 13:18:33 +0530

    Build successful at Local after adding <<testng>> scope as compile

---
+++ src/main/java/basicJavaProg/PrintClass.java
@@ -0,0 +1,11 @@
+package basicJavaProg;
+
+public class PrintClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("First Java Prog for CI");
+
+	}
+
+}
---
+++ src/test/java/basicUnitTest/UnitTestPrint.java
@@ -0,0 +1,13 @@
+package basicUnitTest;
+
+import org.testng.annotations.Test;
+
+public class UnitTestPrint {
+
+	
+	@Test
+	public void printMsg() {
+		System.out.println("First unit test for CI");
+	}
+	
+}
